96fee4d7e722a3953fd00b36403bc8f62c1904db
Francis Lagares <francis.lagares@gmail.com>
2025-02-02 16:43:22 +0100

    feat: loading and not-found page

---
+++ src/app/loading.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+import loader from '@/assets/loader.gif';
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image src={loader} height={100} width={100} alt='Loading...' />
+    </div>
+  );
+};
+
+export default Loading;
